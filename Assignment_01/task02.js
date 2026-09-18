c8e8d84f8b1c24e293541054199a8bb78a46e1e1
shreyashekhar11188 <shreya.25b01011188@abes.ac.in>
2026-09-18 13:37:52 +0530

    task02

--- Assignment_01/task02.js
+++ Assignment_01/task02.js
@@ -7,3 +7,24 @@ const application = new EventEmitter();
 application.on("input", (text) => {
     console.log("Entered text:", text);
 });
+
+// Button click event
+application.on("buttonClick", () => {
+    console.log("Button clicked!");
+});
+
+// Form submit event
+application.on("formSubmit", () => {
+    console.log("Form submitted successfully.");
+});
+
+// Extra custom event: logout
+application.on("logout", (username) => {
+    console.log(`${username} logged out successfully.`);
+});
+
+// Trigger events
+application.emit("input", "Hello Node.js");
+application.emit("buttonClick");
+application.emit("formSubmit");
+application.emit("logout", "Rahul");
